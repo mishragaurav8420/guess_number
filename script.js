b60e5fc1075f220ccfd5054e7d716910c3a8b49d
mishragaurav8420 <mishragaurav8420@gmail.com>
2023-06-29 02:43:15 +0530

    highscore

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // console.log(document.querySelector('.message').textContent)
 // document.querySelector('.message').textContent='hii gaurav mishra'
-const hide_number=Math.trunc(Math.random()*20)+1;
+let hide_number=Math.trunc(Math.random()*20)+1;
 // document.querySelector('.number').textContent=hide_number;
 let count=20;
+let highscore=0;
 document.querySelector('.check').addEventListener('click',function(){
     const number=Number(document.querySelector('.guess').value)
-    console.log(typeof(number))
     if(!number){
         document.querySelector('.message').textContent='No value choosen';
     }
     else if(number === hide_number){
         document.querySelector('.message').textContent="Correct choice";
+        document.querySelector('body').style.backgroundColor='green';
+        if(count > highscore){
+            highscore=count;
+            document.querySelector('.highscore').textContent=highscore;
+        }
+
     }
     else if(number>hide_number){
         document.querySelector('.message').textContent='Too high';
@@ -31,4 +37,13 @@ document.querySelector('.check').addEventListener('click',function(){
             count=0;
         }
     }
-})
+});
+document.querySelector('.again').addEventListener('click',function(){
+    hide_number=Math.trunc(Math.random()*20)+1;
+    count=20; 
+    document.querySelector('.message').textContent='Start Guessing....';
+    document.querySelector('.score').textContent=count;
+    document.querySelector('.number').textContent='?';
+    document.querySelector('.guess').value='';
+    document.querySelector('body').style.backgroundColor='#222';
+});
