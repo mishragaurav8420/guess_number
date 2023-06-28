f5b963f3b7faa147bc8e9c318ae7f1ba5a682e1c
mishragaurav8420 <mishragaurav8420@gmail.com>
2023-06-29 01:45:58 +0530

    add js code

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent='hii gaurav mishra'
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent='hii gaurav mishra'
+const hide_number=Math.trunc(Math.random()*20)+1;
+// document.querySelector('.number').textContent=hide_number;
+let count=20;
+document.querySelector('.check').addEventListener('click',function(){
+    const number=Number(document.querySelector('.guess').value)
+    console.log(typeof(number))
+    if(!number){
+        document.querySelector('.message').textContent='No value choosen';
+    }
+    else if(number === hide_number){
+        document.querySelector('.message').textContent="Correct choice";
+    }
+    else if(number>hide_number){
+        document.querySelector('.message').textContent='Too high';
+        count--;
+        if(count>=0){
+        document.querySelector('.score').textContent=count;}
+        else{
+        count=0;
+        }
+
+    }
+    else if(number<hide_number){
+        document.querySelector('.message').textContent='Too low';
+        count--;
+        if(count>=0){
+        document.querySelector('.score').textContent=count;}
+        else{
+            count=0;
+        }
+    }
+})
